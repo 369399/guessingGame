0e6dfbdf132eb1999668846032d587469cb8d1cc
Lara <369399@mcpsmd.org>
2019-11-14 08:59:38 -0500

    Idk where i am part one

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
 name = input ("what's your name?")
 print( name, " nice to meet you")
+age = int(input ("how old are you"))
+if age < 20:
+    print ("ah i see that youre a teenager")
+else: 
+    print ("an adult i see")
