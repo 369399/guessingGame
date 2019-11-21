360819afa2f48df4d53a64a06cb4c77bba387cf7
Lara <369399@mcpsmd.org>
2019-11-21 09:17:22 -0500

    Figuring it out 1

--- sa/guessingGame.py
+++ sa/guessingGame.py
@@ -5,4 +5,13 @@ guessesTaken = 0
  
 number = random.randint(1, 20)
 print(number, " your number is")
+print('Well, I am thinking of a number between 1 and 20.')
+ 
+while guessesTaken < 6:
+ print('Take a guess.') # There are four spaces in front of print.
+ guess = input()
+ guess = int(guess)
+ 
+ guessesTaken = guessesTaken + 1
+ 
  
